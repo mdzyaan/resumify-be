f3d007572e22c5f2492e4cca714b82a8a031ca8a
Zyaan Mohammed <zyaan@insent.ai>
2020-01-04 23:21:39 +0530

    checking access ip address

--- routes/api/users.js
+++ routes/api/users.js
@@ -84,7 +84,7 @@ router.post("/register", (req, res) => {
 router.post("/login", (req, res) => {
   const { errors, isValid } = validateLoginInput(req.body);
   if (!isValid) {
-    return res.status(400).json(errors);
+    return res.status(400).json(errors); 
   } 
 
   const email = req.body.email.toLowerCase();
